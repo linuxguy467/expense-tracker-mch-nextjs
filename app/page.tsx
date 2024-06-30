17ecb24c4e4072e6fa466d6625c3b2d6e16afb0c
linuxguy467 <me@matthem.codes>
2024-06-30 17:19:11 -0400

    add general ui screen for guest visits

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-const HomePage = () => {
-  return (
+import { currentUser } from '@clerk/nextjs/server';
+import Guest from '@/components/Guest';
+
+const HomePage = async () => {
+  const user = await currentUser();
+
+  return !user ? (
+    <Guest />
+  ) : (
     <main>
-      <h1>Expense Tracker</h1>
+      <h1>Welcome, {user.firstName}</h1>
     </main>
   );
 };
